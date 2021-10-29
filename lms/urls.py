1fc916ea1ac503c56ca163672e378ff0110ce3d5
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-10-29 21:22:54 +0600

    project initial setup

--- lms/urls.py
+++ lms/urls.py
@@ -1,7 +1,7 @@
-"""lms URL Configuration
+"""core URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,9 +13,38 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+   
+    # prometheus url
+    path('', include('django_prometheus.urls')),
+
+    # admin urls 
+    path(f'{settings.ADMIN_URL}/', admin.site.urls),
+
+    # drf api auth
+    path('api-auth/', include('rest_framework.urls')),
 ]
+
+# serve media files in development environment
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
+
+# debug toolbar
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
+
+# admin site customizations
+admin.sites.AdminSite.site_header = "SHOSENLMS Administration"
+admin.sites.AdminSite.site_title = "SHOSENLMS Administration"
+admin.sites.AdminSite.index_title = "SHOSENLMS Admin Panel"
