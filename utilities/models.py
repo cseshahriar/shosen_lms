90bb7b03ef3f3a9abfe584c923326d6bfc947187
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-11-04 23:22:34 +0600

    chat model created

--- utilities/models.py
+++ utilities/models.py
@@ -121,3 +121,18 @@ class Application(models.Model):
 
     def __str__(self):
         return str(self.detail[0:30])
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='sender'
+    )
+    receiver = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='receiver'
+    )
+    message = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.sender} - {self.receiver}"
