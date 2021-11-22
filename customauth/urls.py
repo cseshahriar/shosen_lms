c1868e7730bceec087b1fba958f7597ab010ce52
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-11-22 20:33:22 +0600

    dashboard init templating

--- customauth/urls.py
+++ customauth/urls.py
@@ -4,11 +4,7 @@ from .views import LMSLoginView, logout_view, DashboardView
 urlpatterns = [
     # authentication urls
     path('login/', LMSLoginView.as_view(), name='lms_login'),
-    path(
-        'logout/',
-        logout_view,
-        name='lms_logout'
-    ),
+    path('logout/', logout_view, name='lms_logout'),
 
     # dashboard urls
     path('dashboard/', DashboardView.as_view(), name='lms_dashboard'),
